0a9013319fcd8a20de9e11d20d63a7cf3a0bc586
islavstan <islavstan@gmail.com>
2017-04-28 21:51:05 +0300

    1

---
+++ android/src/com/islavstan/lunahod/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.islavstan.lunahod;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.islavstan.lunahod.Lunahod;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Lunahod(), config);
+	}
+}
---
+++ html/src/com/islavstan/lunahod/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.islavstan.lunahod.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.islavstan.lunahod.Lunahod;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Lunahod();
+        }
+}
